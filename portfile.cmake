f2f41f4b8b8a0e8b36255cdce1e803f026a5b1f6
pfhgil <stukalovila76@gmail.com>
2024-03-31 04:24:36 +0300

    UPD

--- portfile.cmake
+++ portfile.cmake
@@ -1,27 +1,27 @@
 vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
+set(VCPKG_BUILD_TYPE release)
+
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO Pixelfield-ru/SungearEngineSDK
         REF "${VERSION}"
-        SHA512 106c5f09b1ff95f90a9b981404c4d5682ec0a5dec3222d8027fc7a19c936efb581038a7235fad6a9e932e947053e1d9a11a229370245fa448be62dbf131e47db
+        SHA512 6bd7fa6f6b0c785898e41edfac9c5134cf58b3f6a72bbd433da22d03116334a70a25d804391f9c2c214a4cc22517ec39ff814e62e1dabade74bb46381759c453
         HEAD_REF main
 )
 
 vcpkg_cmake_configure(
         SOURCE_PATH "${SOURCE_PATH}"
         OPTIONS
-        -DCMAKE_BUILD_TYPE=Release
+        -DCMAKE_BUILD_TYPE=release
 )
 
 vcpkg_cmake_install()
 
 #vcpkg_cmake_config_fixup(PACKAGE_NAME "sungearengine")
-#vcpkg_cmake_config_fixup(CONFIG_PATH bin/cmake/sungearengine)
+vcpkg_cmake_config_fixup(CONFIG_PATH share/sungearengine-sdk)
 vcpkg_fixup_pkgconfig()
-vcpkg_cmake_config_fixup()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 # file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 #[[file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
